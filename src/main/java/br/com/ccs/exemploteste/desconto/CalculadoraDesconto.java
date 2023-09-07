acb01d622d4a2a09cf87f5efa99b8dd0775ea741
Cleber Souza <ccs1201@gmail.com>
2023-09-07 20:54:28 -0300

    #First Commit

--- src/main/java/br/com/ccs/exemploteste/desconto/CalculadoraDesconto.java
+++ src/main/java/br/com/ccs/exemploteste/desconto/CalculadoraDesconto.java
@@ -1,2 +1,43 @@
-package br.com.ccs.exemploteste.desconto;public class CalculadoraDesconto {
+package br.com.ccs.exemploteste.desconto;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class CalculadoraDesconto {
+
+
+    public BigDecimal aplicarDesconto(BigDecimal valor, Float percentual) {
+
+        validarDescontoMaximo(percentual);
+        validarDescontoMinimimo(percentual);
+
+        return valor.multiply(BigDecimal.valueOf(1 - (percentual / 100f))).setScale(2, RoundingMode.HALF_UP);
+    }
+
+
+    /**
+     * Desconto não pode ser maior que 100%
+     *
+     * @param percentual
+     */
+    private void validarDescontoMaximo(Float percentual) {
+
+        if (percentual > 100f) {
+            throw new RuntimeException("Desconto não pode maior que cem por cento");
+        }
+
+    }
+
+    /**
+     * Desconto não pode ser menor que 1%
+     *
+     * @param percentual
+     */
+    private void validarDescontoMinimimo(Float percentual) {
+        if (percentual < 1f) {
+            throw new RuntimeException("Desconto não pode ser menor que um por cento");
+        }
+    }
+
+
 }
--- src/test/java/br/com/ccs/exemploteste/desconto/CalculadoraDescontoTest.java
+++ src/test/java/br/com/ccs/exemploteste/desconto/CalculadoraDescontoTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.ccs.exemploteste.desconto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+@ExtendWith(MockitoExtension.class)
 class CalculadoraDescontoTest {
-  
+    private final BigDecimal valor = BigDecimal.valueOf(100);
+    @InjectMocks
+    private CalculadoraDesconto calculadoraDesconto;
+
+    @Test
+    void aplicarDescontoMaiorQuePermitido() {
+        Assertions.assertThrows(RuntimeException.class,
+                () -> calculadoraDesconto.aplicarDesconto(valor, 101f));
+    }
+
+    @Test
+    void aplicarDescontoMenorQuePermitido() {
+        Assertions.assertThrows(RuntimeException.class,
+                () -> calculadoraDesconto.aplicarDesconto(valor, 0.9f));
+    }
+
+    @Test
+    void aplicarDescdontoSucesso() {
+        var expected = BigDecimal.valueOf(90).setScale(2);
+        var actual = calculadoraDesconto.aplicarDesconto(valor, 10f);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
